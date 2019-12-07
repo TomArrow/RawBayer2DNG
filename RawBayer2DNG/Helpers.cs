925e6f7ab5cf93278c9c842e63ff891de5ad02e3
Hans Doof <hans@doof.de>
2019-12-07 22:27:03 +0100

    fucking bullshit 16bpp grayscale doesn't work, need to find other way

--- RawBayer2DNG/Helpers.cs
+++ RawBayer2DNG/Helpers.cs
@@ -26,5 +26,23 @@ namespace RawBayer2DNG
                 return bitmapimage;
             }
         }
+
+        static public int getStride(int byteWidth)
+        {
+        
+            int modulo4 = byteWidth % 4;
+            return byteWidth + (modulo4 > 0 ? (4 - modulo4) : 0);
+        }
+
+        static public byte[] PadLines(byte[] bytes, int height, int width,int newStride,int bytesPerPixel)
+        {
+            //The old and new offsets could be passed through parameters,
+            //but I hardcoded them here as a sample.
+            //var newStride = width + (4-width%4);
+            var newBytes = new byte[newStride * height];
+            for (var i = 0; i < height; i++)
+                Buffer.BlockCopy(bytes, width *bytesPerPixel* i, newBytes, newStride * i, width*bytesPerPixel);
+            return newBytes;
+        }
     }
 }
--- RawBayer2DNG/MainWindow.xaml.cs
+++ RawBayer2DNG/MainWindow.xaml.cs
@@ -15,9 +15,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using Shapes = System.Windows.Shapes;
+using Forms = System.Windows.Forms;
 
 using System.Drawing;
+using Imaging = System.Drawing.Imaging;
 using Orientation = BitMiracle.LibTiff.Classic.Orientation;
+using System.Runtime.InteropServices;
 
 namespace RawBayer2DNG
 {
@@ -26,19 +29,9 @@ namespace RawBayer2DNG
     /// </summary>
     public partial class MainWindow : Window
     {
-
-
-
-        /*private const TiffTag TIFFTAG_ASCIITAG = (TiffTag)666;
-        private const TiffTag TIFFTAG_LONGTAG = (TiffTag)667;
-        private const TiffTag TIFFTAG_SHORTTAG = (TiffTag)668;
-        private const TiffTag TIFFTAG_RATIONALTAG = (TiffTag)669;
-        private const TiffTag TIFFTAG_FLOATTAG = (TiffTag)670;
-        private const TiffTag TIFFTAG_DOUBLETAG = (TiffTag)671;
-        private const TiffTag TIFFTAG_BYTE = (TiffTag)672;*/
+        
         private const TiffTag TIFFTAG_CFAREPEATPATTERNDIM = (TiffTag)33421;
         private const TiffTag TIFFTAG_CFAPATTERN = (TiffTag)33422;
-
 
         private static Tiff.TiffExtendProc m_parentExtender;
 
@@ -46,21 +39,13 @@ namespace RawBayer2DNG
         {
             TiffFieldInfo[] tiffFieldInfo =
             {
-                //new TiffFieldInfo(TIFFTAG_ASCIITAG, -1, -1, TiffType.ASCII, FieldBit.Custom, true, false, "MyTag"),
                 new TiffFieldInfo(TIFFTAG_CFAREPEATPATTERNDIM, 2, 2, TiffType.SHORT, FieldBit.Custom, false, false, "CFARepeatPatternDim"),
                 new TiffFieldInfo(TIFFTAG_CFAPATTERN, 4, 4, TiffType.BYTE, FieldBit.Custom, false, false, "CFAPattern"),
-                /*new TiffFieldInfo(TIFFTAG_SHORTTAG, 2, 2, TiffType.SHORT, FieldBit.Custom, false, true, "ShortTag"),
-                new TiffFieldInfo(TIFFTAG_LONGTAG, 2, 2, TiffType.LONG, FieldBit.Custom, false, true, "LongTag"),
-                new TiffFieldInfo(TIFFTAG_RATIONALTAG, 2, 2, TiffType.RATIONAL, FieldBit.Custom, false, true, "RationalTag"),
-                new TiffFieldInfo(TIFFTAG_FLOATTAG, 2, 2, TiffType.FLOAT, FieldBit.Custom, false, true, "FloatTag"),
-                new TiffFieldInfo(TIFFTAG_DOUBLETAG, 2, 2, TiffType.DOUBLE, FieldBit.Custom, false, true, "DoubleTag"),
-                new TiffFieldInfo(TIFFTAG_BYTE, 2, 2, TiffType.BYTE, FieldBit.Custom, false, true, "ByteTag"),*/
             };
 
-            /*
-             *
+            /* Reference code copied from C++ version of LibTiff (not yet implemented in LibTiff.NET)
              *{ TIFFTAG_CFAREPEATPATTERNDIM, 2, 2, TIFF_SHORT, 0, TIFF_SETGET_C0_UINT16, TIFF_SETGET_UNDEFINED,	FIELD_CUSTOM, 0,	0,	"CFARepeatPatternDim", NULL },
-	{ TIFFTAG_CFAPATTERN,	4, 4,	TIFF_BYTE, 0, TIFF_SETGET_C0_UINT8, TIFF_SETGET_UNDEFINED, FIELD_CUSTOM, 0,	0,	"CFAPattern" , NULL},
+	            { TIFFTAG_CFAPATTERN,	4, 4,	TIFF_BYTE, 0, TIFF_SETGET_C0_UINT8, TIFF_SETGET_UNDEFINED, FIELD_CUSTOM, 0,	0,	"CFAPattern" , NULL},
              */
 
             tif.MergeFieldInfo(tiffFieldInfo, tiffFieldInfo.Length);
@@ -103,7 +88,7 @@ namespace RawBayer2DNG
                     output.SetField(TiffTag.COMPRESSION, Compression.ADOBE_DEFLATE);
                     output.SetField(TiffTag.PHOTOMETRIC, Photometric.MINISBLACK);
                     output.SetField(TiffTag.FILLORDER, FillOrder.MSB2LSB);
-                    //output.SetField(TiffTag.COMPRESSION, Compression.LZW);
+                    //output.SetField(TiffTag.COMPRESSION, Compression.LZW); //LZW doesn't work with DNG apparently
                     //output.SetField(TiffTag.COMPRESSION, Compression.NONE);
                     output.SetField(TiffTag.PLANARCONFIG, PlanarConfig.CONTIG);
 
@@ -119,31 +104,85 @@ namespace RawBayer2DNG
                     output.SetField(TiffTag.SUBFILETYPE, 0);
                     output.SetField(TiffTag.MAKE, "blah");
                     output.SetField(TiffTag.MODEL, "blah");
-                    //output.SetField(TiffTag.SAMPLESPERPIXEL, 1);
                     output.SetField(TiffTag.SOFTWARE, "Blah");
-                    //output.SetField(TiffTag.DNGVERSION, "\001\001\0\0");
                     output.SetField(TiffTag.DNGVERSION, "\x1\x4\x0\x0");
-                    //output.SetField(TiffTag.DNGBACKWARDVERSION, "\001\0\0\0");
                     output.SetField(TiffTag.DNGBACKWARDVERSION, "\x1\x4\x0\x0");
                     output.SetField(TiffTag.UNIQUECAMERAMODEL, "blah");
                     output.SetField(TiffTag.COLORMATRIX1, 9, cam_xyz);
                     output.SetField(TiffTag.ASSHOTNEUTRAL, 3, neutral);
                     output.SetField(TiffTag.CALIBRATIONILLUMINANT1, 21);
                     output.SetField(TiffTag.ORIGINALRAWFILENAME, ofd.FileName);
-                    //output.SetField(TiffTag.BITSPERSAMPLE, 3, bpp);
                     output.SetField(TiffTag.PHOTOMETRIC, 32803);
                     output.SetField(TiffTag.SAMPLESPERPIXEL, 1);
-                    //output.SetField(TiffTag.EXIF_CFAPATTERN, 4, "\001\002\000\001");
                     output.SetField(TiffTag.EXIF_CFAPATTERN, 4, "\x1\x0\x2\x1");
-                    //output.SetField(TiffTag.CFAPLANECOLOR, );
                     output.SetField(TIFFTAG_CFAREPEATPATTERNDIM, bayerpatterndimensions);
                     output.SetField(TIFFTAG_CFAPATTERN, "\x1\x0\x2\x1"); //0=Red, 1=Green,   2=Blue,   3=Cyan,   4=Magenta,   5=Yellow,   and   6=White
-                    //output.SetField(TiffTag.CFA, 4, "\001\002\000\001");
+
+                    // Maybe use later if necessary:
+                    //output.SetField(TiffTag.SAMPLESPERPIXEL, 1);
+                    //output.SetField(TiffTag.BITSPERSAMPLE, 3, bpp);
                     //output.SetField(TiffTag.LINEARIZATIONTABLE, 256, linearizationTable);
                     //output.SetField(TiffTag.WHITELEVEL, 1);
 
                     output.WriteEncodedStrip(0, buff, width * height * 2);
                 }
+            }
+        }
+
+
+        string sourceFolder = null;
+        string[] filesInSourceFolder = null;
+
+
+        private void BtnLoadRAWFolder_Click(object sender, RoutedEventArgs e)
+        {
+
+            var fbd = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            bool? result = fbd.ShowDialog();
+
+            if (result == true && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                sourceFolder = fbd.SelectedPath;
+                filesInSourceFolder = Directory.GetFiles(fbd.SelectedPath,"*.raw");
+                currentImagNumber.Text = "1";
+                totalImageCount.Text = filesInSourceFolder.Count().ToString();
+                slide_currentFile.Maximum = filesInSourceFolder.Count();
+                slide_currentFile.Minimum = 1;
+                slide_currentFile.Value = 1;
+            }
+        }
+
+        private void Slide_currentFile_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            int width = int.Parse(rawWidth.Text);
+            int height = int.Parse(rawHeight.Text);
+
+            int sliderNumber = (int)slide_currentFile.Value;
+            int index = sliderNumber - 1;
+            string selectedRawFile = filesInSourceFolder[index];
+            if (!File.Exists(selectedRawFile))
+            {
+                MessageBox.Show("weirdo error, apparently file "+selectedRawFile+" (no longer?) exists");
+                return;
+            } else
+            {
+
+                byte[] buff = File.ReadAllBytes(selectedRawFile);
+                int byteWidth = width * 2;
+                int newStride = Helpers.getStride(byteWidth);
+                byte[] newbytes = Helpers.PadLines(buff, height, width, newStride,2);
+                Bitmap manipulatedImage = new Bitmap(width, height, Imaging.PixelFormat.Format16bppGrayScale);
+                Imaging.BitmapData pixelData = manipulatedImage.LockBits(new Rectangle(0, 0, width, height), Imaging.ImageLockMode.WriteOnly, Imaging.PixelFormat.Format16bppGrayScale);
+
+                //Bitmap im = new Bitmap(width, height, newStride, Imaging.PixelFormat.Format16bppGrayScale,  Marshal.UnsafeAddrOfPinnedArrayElement(newbytes, 0));
+
+                System.Runtime.InteropServices.Marshal.Copy(newbytes, 0, pixelData.Scan0,newbytes.Count());
+                //im.GetPixel(1, 1);
+                //im.GetPixel(2447, 2047);
+                //pixelData.
+                manipulatedImage.UnlockBits(pixelData);
+                mainPreview.Source = Helpers.BitmapToImageSource(manipulatedImage);
             }
         }
     }
