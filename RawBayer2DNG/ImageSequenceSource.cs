3e474d5365569d715876d8e1c91c80f5a71d698e
Tom Arrow <davorfilms@googlemail.com>
2020-12-29 14:31:02 +0100

    did some preparations for abstracting input sequence type to allow to add .seq support later

---
+++ RawBayer2DNG/ImageSequenceSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawBayer2DNG
+{
+    abstract class ImageSequenceSource
+    {
+
+        public enum ImageSequenceSourceType { INVALID, DNG, STREAMPIX_SEQ };
+
+        private ImageSequenceSourceType sourceType = ImageSequenceSourceType.INVALID; 
+
+        abstract public int getWidth();
+        abstract public int getHeight();
+        abstract public byte[] getBayerPattern();
+        abstract public byte[] getRawImageData(int index);
+        abstract public int getImageCount();
+        public ImageSequenceSourceType getSourceType()
+        {
+            return sourceType;
+        }
+    }
+}
---
+++ RawBayer2DNG/ImageSequenceSources/DNGSequenceSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawBayer2DNG.ImageSequenceSources
+{
+    class DNGSequenceSource : ImageSequenceSource
+    {
+
+        public int width;
+        public int height;
+        public byte[] bayerPattern;
+        public string[] paths;
+
+        ImageSequenceSourceType sourceType = ImageSequenceSourceType.DNG;
+
+        public DNGSequenceSource(int theWidth, int theHeight, byte[] theBayerPattern, string[] thePaths)
+        {
+            width = theWidth;
+            height = theHeight;
+            bayerPattern = theBayerPattern;
+            paths = thePaths;
+        }
+
+        override public int getWidth()
+        {
+            return width;
+        }
+        override public int getHeight()
+        {
+            return height;
+        }
+        override public byte[] getBayerPattern()
+        {
+            return bayerPattern;
+        }
+        override public byte[] getRawImageData(int index)
+        {
+            return File.ReadAllBytes(paths[index]);
+        }
+        override public int getImageCount()
+        {
+            return paths.Length;
+        }
+    }
+}
