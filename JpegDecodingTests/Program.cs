9036ead197dfa7a59b6bac19c765dddb9f291715
Tom Arrow <davorfilms@googlemail.com>
2021-01-30 06:14:10 +0100

    lossless JPEG encode kinda works! but its stretched... hmm

--- JpegDecodingTests/Program.cs
+++ JpegDecodingTests/Program.cs
@@ -13,7 +13,8 @@ namespace JpegDecodingTests
         static void Main(string[] args)
         {
             //testAdobeJPG();
-            testCRIJPG();
+            //testCRIJPG();
+            testAdobeJPGEncode();
         }
 
 
@@ -65,6 +66,47 @@ namespace JpegDecodingTests
 
 
             File.WriteAllBytes("sof3-decodetest.raw", tileBuff);
+        }
+        private static void testAdobeJPGEncode()
+        {
+
+            int tileWidth = 256;
+            int tileHeight = 240;
+
+
+            byte[] rawReferenceData = File.ReadAllBytes("rawToEncode-DNG.raw");
+            UInt16[] rawReferenceDataUInt16 = new UInt16[rawReferenceData.Length / 2];
+            for(int i = 0; i < rawReferenceDataUInt16.Length; i++)
+            {
+                rawReferenceDataUInt16[i] = BitConverter.ToUInt16(rawReferenceData, i * 2);
+            }
+
+            dng_stream whatever = new dng_stream();
+            DNGLosslessEncoder.EncodeLosslessJPEG(rawReferenceDataUInt16, (uint)tileHeight, (uint)tileWidth/2, 2, 16, tileWidth, 1, whatever);
+
+            File.WriteAllBytes("encodedTest.jpg", whatever.toByteArray());
+
+
+            // Try decode again
+            JpegDecoder jpegLibraryDecoder = new JpegDecoder();
+
+            byte[] rawTileBuffer = File.ReadAllBytes("encodedTest.jpg");
+
+
+
+            byte[] tileBuff = new byte[tileWidth * tileHeight * 2];
+
+
+            ReadOnlyMemory<byte> rawTileReadOnlyMemory;
+            rawTileReadOnlyMemory = new ReadOnlyMemory<byte>(rawTileBuffer);
+            jpegLibraryDecoder.SetInput(rawTileReadOnlyMemory);
+            //jpegLibraryDecoder.SetFrameHeader()
+            jpegLibraryDecoder.Identify(); // fails to identify. missing markers or whatever: Failed to decode JPEG data at offset 91149. No marker found.'
+            jpegLibraryDecoder.SetOutputWriter(new JpegDecode.JpegBufferOutputWriterGreaterThan8Bit(tileWidth / 2, tileHeight, jpegLibraryDecoder.Precision, 2, tileBuff, 16));
+            jpegLibraryDecoder.Decode();
+
+
+            File.WriteAllBytes("sof3-encodedecodetest.raw", tileBuff);
         }
     }
 }
