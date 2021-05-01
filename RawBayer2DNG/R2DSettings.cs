da50321bdb2d131553ac0ae7d4ce1ed7740adcdf
Tom Arrow <davorfilms@googlemail.com>
2021-05-01 20:04:13 +0200

    starting to work on presets

---
+++ RawBayer2DNG/R2DSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawBayer2DNG
+{
+    class R2DSettings : PresetManager.AppSettings
+    {
+
+    }
+}
